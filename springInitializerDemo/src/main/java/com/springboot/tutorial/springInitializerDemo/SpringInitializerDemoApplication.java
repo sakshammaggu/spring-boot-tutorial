5af69d5027334844c273713ceb291b74a2f5b716
sakshammaggu <sakshammaggu80@gmail.com>
2024-11-24 02:59:41 +0530

    first commit

---
+++ springInitializerDemo/src/main/java/com/springboot/tutorial/springInitializerDemo/SpringInitializerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.tutorial.springInitializerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitializerDemoApplication.class, args);
+	}
+
+}
---
+++ springInitializerDemo/src/test/java/com/springboot/tutorial/springInitializerDemo/SpringInitializerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.tutorial.springInitializerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
